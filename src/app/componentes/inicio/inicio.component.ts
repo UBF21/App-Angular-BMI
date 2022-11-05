ddb2e7f89248b91751c3832d55837911873c54ec
UBF21 <flashgames104@gmail.com>
2022-11-05 13:57:39 -0500

    culminacion de apliacion de calculo BMI

--- src/app/componentes/inicio/inicio.component.ts
+++ src/app/componentes/inicio/inicio.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { getTextOfJSDocComment } from 'typescript';
 
 @Component({
   selector: 'app-inicio',
@@ -7,9 +9,48 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InicioComponent implements OnInit {
 
-  constructor() { }
+  edad: number = 25;
+  peso: number = 20;
+  altura: number = 170;
+  sexo: string = "Masculino";
+
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
   }
 
+  restarPeso(): void {
+    this.peso--;
+  }
+  sumarPeso(): void {
+    this.peso++;
+  }
+
+  restarEdad(): void {
+    this.edad--;
+  }
+
+  sumarEdad(): void {
+    this.edad++;
+  }
+
+  femenino(): void {
+    this.sexo = "Femenino";
+  }
+
+  masculino(): void {
+    this.sexo = "Masculino";
+
+  }
+
+  cambiarAltura(event: any) {
+    this.altura = event.target.value;
+  }
+
+  calcularBMI(): void {
+    const BMI = this.peso / Math.pow((this.altura / 100), 2);
+    console.log(BMI.toFixed(1));
+    this.router.navigate(["/resultado",BMI.toFixed(1)]);
+  }
 }
+
